22d0111c40f0b2df9a5b725b0614650c69b7cee5
Edoardo Guido <edoardo.guido.93@gmail.com>
2018-12-27 19:42:40 +0100

    Added PHP server to handle session data save and image printing; updated sketch code
    Still lacking touch support

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 var width;
 var height;
-var cursor;
 
 var scrollSpeed,
     mappedSpeed;
 
 // var color;
-var speed = 1;
+var speed = 2;
 var scrollHistory = [];
 var scrolled = 0;
 var scrolledcm;
-var conversionUnit = 0.0026458333;
+var CONVERSION_VALUE = 0.0026458333;
+var color;
+var stroke;
+var strweight = 16;
 
 // variable to store canvas for later write on image file
-var cnv;
-var toSave = false;
-var saved = false;
+var cnv, gph;
 
 function setup() {
     cnv = createCanvas(window.innerWidth / 3, window.innerHeight / 1.1);
@@ -23,90 +23,121 @@ function setup() {
     height = cnv.height;
     width = cnv.width;
 
-    cursor = document.getElementById('cursor');
-    cursor.style.top = height + 'px';
+    gph = createGraphics(width, height);
+    gph.background(255);
 }
-
 
 function draw() {
 
     // this is before everything so that we can draw with a transparent background
+    clear();
+    gph.background(255);
+
     if (scrollHistory.length > height / speed) {
-        clear();
         scrollHistory.splice(0, 1);
-        if (saved == false) {
-            toSave = true;
-        }
     }
 
     translate(width / 2, 0);
 
+    beginShape();
     noFill();
-    // fill(color);
     for (var i = 0; i < scrollHistory.length; i++) {
+        color = map(scrollHistory[i], 1, window.innerWidth / 4, 80, 0);
+        strokeColor = map(scrollHistory[i], 0, cnv.width - (strweight * 10), 2, strweight);
+
         var x = scrollHistory[i];
         var y = i * speed;
-        stroke(0);
-        strokeWeight(12);
+
+        stroke(color);
+        strokeWeight(strokeColor);
         line(-x, y, x, y);
+
+        gph.stroke(color);
+        gph.strokeWeight(strokeColor);
+        // manual translation because translate function seems to be 
+        // in conflict with original canvas translation 
+        gph.line(-x + (gph.width / 2), y, x + (gph.width / 2), y);
     }
+    endShape();
 
     noLoop();
-}
-
-function mouseWheel(e) {
-
-    updateScroll(e);
-
-    if (toSave == true && saved == false) {
-        toSave = false;
-        saved = true;
-
-        // save(cnv, 'it-werks.png');
-        // saveToServer();
-
-    } else return;
-}
-
-// function touchMoved(e) {
-//     updateScroll(e);
-// }
-
-function mouseClicked() {
-    save(cnv, 'traccia.png');
 }
 
 function updateScroll(e) {
 
     scrollSpeed = abs(e.delta);
-    mappedSpeed = constrain(scrollSpeed, 1, width);
+    mappedSpeed = constrain(scrollSpeed, 0, cnv.width - (strweight * 10));
     scrollHistory.push(mappedSpeed);
 
-    // color = map(mappedSpeed, 1, window.innerWidth / 4, 135, 0);
-
     scrolled += 1;
-    scrolledcm = scrolled * conversionUnit;
-    print(scrolledcm);
-
-    // switch (true) {
-    //     case scrolledcm < 1:
-    //         document.getElementById('scroll-counter').innerHTML = `You have scrolled ${(scrolledcm).toFixed(0)} millimeters.`;
-    //         break;
-    //     case scrolledcm >= 1:
-    //         document.getElementById('scroll-counter').innerHTML = `You have scrolled ${scrolledcm.toFixed(2)} centimeters.`
-    //         break;
-    // }
+    scrolledcm = scrolled * CONVERSION_VALUE;
 
     document.getElementById('scroll-counter').innerHTML = `You have scrolled ${(scrolledcm * 10).toFixed(0)} centimeters.`
 
     loop();
 }
 
+function mouseWheel(e) {
+    updateScroll(e);
+}
+
+// TODO - Touch support
+// function touchMoved(e) {
+//     updateScroll(e);
+// }
+
+function mouseClicked() {
+    var elToSave = gph.canvas;
+    var canvasData = elToSave.toDataURL('image/png');
+    var headerData = 'data:image/png;base64,';
+    canvasData = canvasData.replace(headerData, '')
+
+    saveToServer(canvasData, 'server_side/save_image.php');
+}
+
 function windowResized() {
     resizeCanvas(window.innerWidth, window.innerHeight / 1.1);
 }
 
-function saveToServer() {
-    var mycanvas = document.getElementById("defaultCanvas0"); //get your canvas
-    var image = mycanvas.toDataURL("image/png"); //Convert the canvas to image, currently converting to .png
+function saveToServer(source, targetOp) {
+    var http = new XMLHttpRequest();
+    http.open('POST', targetOp, true);
+
+    // http.setRequestHeader('Content-type', 'image/png');
+
+    http.onreadystatechange = function () { //Call a function when the state changes.
+        if (http.readyState == 4 && http.status == 200) {
+            print(http.response);
+        }
+    }
+
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + "-" + today.getMinutes() + "-" + today.getSeconds();
+
+    var obj = {
+        "meta-data": date + '_' + time,
+        "scroll-data": scrollHistory,
+        "img-data": source
+    }
+    var json = JSON.stringify(obj);
+
+    http.send(json);
+}
+
+function floydSteinberg(sb, w, h) // source buffer, width, height
+{
+    for (var i = 0; i < h; i++)
+        for (var j = 0; j < w; j++) {
+            var ci = i * w + j; // current buffer index
+            var cc = sb[ci]; // current color
+            var rc = (cc < 128 ? 0 : 255); // real (rounded) color
+            var err = cc - rc; // error amount
+            sb[ci] = rc; // saving real color
+            if (j + 1 < w) sb[ci + 1] += (err * 7) >> 4; // if right neighbour exists
+            if (i + 1 == h) continue; // if we are in the last line
+            if (j > 0) sb[ci + w - 1] += (err * 3) >> 4; // bottom left neighbour
+            sb[ci + w] += (err * 5) >> 4; // bottom neighbour
+            if (j + 1 < w) sb[ci + w + 1] += (err * 1) >> 4; // bottom right neighbour
+        }
 }
--- server_side/scroll_printer.py
+++ server_side/scroll_printer.py
@@ -1,7 +1,7 @@
 #!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
-
+#
 # pip2 install libusb qrcode pyusb serial paramiko
-
+#
 # bEndpointAddress     0x81  EP 1 IN
 # bEndpointAddress     0x03  EP 3 OUT
 
@@ -17,23 +17,22 @@ endpoint_OUT    = 0x03
 
 p = Usb(VENDOR_ID, PRODUCT_ID, iInterface, endpoint_IN, endpoint_OUT)
 
-p.image('traccia.png')
+p.image('traccia_a.png')
 
-p.text('\n')
-p.set(align='CENTER', density=1)
-p.text('This is your digital trace.\n')
-p.text('\n')
+# p.image('traccia1.png')
+# p.image('traccia2.png')
 
-p.set(align='CENTER', font='b', width=2, height=2, density=1)
-p.text('You have scrolled\n')
-p.text('46 centimeters far.\n')
-p.text('\n')
+# p.text('\n\n')
+# p.set(align='CENTER', density=1)
+# p.text('Your digital trace is\n')
+# p.text('46 centimeters long.\n')
+# p.text('\n')
 
-p.set(align='CENTER', density=1)
-p.text('Research has shown that\n')
-p.text('the average user scrolls\n')
+# p.set(align='CENTER', density=1)
+# p.text('Research has shown that\n')
+# p.text('the average user scrolls\n')
 
-p.set(align='CENTER', font='b', width=2, height=2, density=1)
-p.text('90 meters each day.\n')
+# p.set(align='CENTER', font='b', width=2, height=2, density=1)
+# p.text('90 meters each day.\n')
 
 p.cut(mode='PART')
